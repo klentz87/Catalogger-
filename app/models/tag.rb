422e30ddb80ea23b945dd41feb52abe3650500ff
Kris E. Lentz <kris.e.lentz@gmail.com>
2018-02-27 10:33:57 -0600

    starting tags again

--- app/models/tag.rb
+++
@@ -1,4 +0,0 @@
-class Tag < ApplicationRecord
-	has_many :book_tags
-	has_many :books, through: :book_tags
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227154210) do
-
-  create_table "book_tags", force: :cascade do |t|
-    t.integer "book_id"
-    t.integer "tag_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180227031259) do
 
   create_table "books", force: :cascade do |t|
     t.string "isbn"
@@ -39,12 +32,6 @@ ActiveRecord::Schema.define(version: 20180227154210) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "phrase"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

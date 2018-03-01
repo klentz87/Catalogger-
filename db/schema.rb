5b24fc8635a84dae63977335f75242cd5f845ab2
Kris E. Lentz <kris.e.lentz@gmail.com>
2018-03-01 08:23:37 -0600

    alter migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227031259) do
+ActiveRecord::Schema.define(version: 20180227163543) do
+
+  create_table "book_hashtags", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "hashtag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "isbn"
@@ -32,6 +39,12 @@ ActiveRecord::Schema.define(version: 20180227031259) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hashtags", force: :cascade do |t|
+    t.string "phrase"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

c89b7ad587ad9b5d95b6b540e909e617c02cc9d6
Kris E. Lentz <kris.e.lentz@gmail.com>
2018-03-01 09:28:38 -0600

    pushing to heroku

--- db/migrate/20180225220310_change_publication_date_to_integer.rb
+++
@@ -1,5 +0,0 @@
-class ChangePublicationDateToInteger < ActiveRecord::Migration[5.1]
-  def change
-  	change_column :books, :publication_date, 'integer USING CAST(publication_date AS integer)'
-  end
-end

446af1190685b44568017a9908ccdbc22085b16b
Kris E. Lentz <kris.e.lentz@gmail.com>
2018-03-01 08:06:11 -0600

    updating heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :genres
   resources :books
 
-  root "books#new"
+  root "books#home"
+
+  get "new" => "books#new"
 
   get "genres" => "genres#index"
 
